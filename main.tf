06b65ef661742d4017dcf232c539e0eb0333caa9
dhwarakesh <dhwarakesh31@gmail.com>
2025-04-24 18:12:25 -0400

    Created VPC module

---
+++ main.tf
@@ -0,0 +1,18 @@
+module "vpc" {
+  source = "./modules/vpc"
+
+  availability_zones     = var.availability_zones
+  backend_subnets        = var.backend_subnets
+  db_subnets             = var.db_subnets
+  default_route_table_id = var.default_route_table_id
+  default_vpc_cidr       = var.default_vpc_cidr
+  default_vpc_id         = var.default_vpc_id
+  env                    = var.env
+  frontend_subnets       = var.frontend_subnets
+  public_subnets         = var.public_subnets
+  vpc_cidr_block         = var.vpc_cidr_block
+}
+
+
+
+
