b305b9d07b3bd1991a255875e08b1403dacb08ab
dhwarakesh <dhwarakesh31@gmail.com>
2025-04-24 20:26:47 -0400

    Created RDS module

---
+++ variables.tf
@@ -0,0 +1,14 @@
+variable "availability_zones" {}
+variable "backend_subnets" {}
+variable "db_subnets" {}
+variable "default_route_table_id" {}
+variable "default_vpc_cidr" {}
+variable "default_vpc_id" {}
+variable "env" {}
+variable "frontend_subnets" {}
+variable "public_subnets" {}
+variable "vpc_cidr_block" {}
+
+variable "rds" {}
+
+variable "kms_key_id" {}
